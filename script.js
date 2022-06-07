ba3681b5e90143237efc8d148cb9adfdf2dcb3c6
Guyddo <guyddogl@gmail.com>
2022-06-07 17:10:17 -0300

    Requisito 18

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const buttonLogin = document.getElementById('submit-login');
 const inputEmail = document.getElementById('email');
 const inputPassword = document.getElementById('password');
+const checkAgreement = document.getElementById('agreement');
+const buttonEnviar = document.getElementById('submit-btn');
 
+// Requisito 3
 function logUserIn() {
   const email = inputEmail.value;
   const password = inputPassword.value;
@@ -13,3 +16,14 @@ function logUserIn() {
 }
 
 buttonLogin.addEventListener('click', logUserIn);
+
+// Requisito 18
+function agreement() {
+  if (checkAgreement.checked === true) {
+    buttonEnviar.removeAttribute('disabled');
+  } else {
+    buttonEnviar.setAttribute('disabled', 'disabled');
+  }
+}
+
+checkAgreement.addEventListener('click', agreement);
